e4f1796b245ea231406d692152b9ec62ac4f502e
Gameovernin <gwenn16.np@gmail.com>
2018-08-06 16:35:23 +0800

    Added Crud

--- laravelFirstWebsite/app/Http/Controllers/CrushesController.php
+++ laravelFirstWebsite/app/Http/Controllers/CrushesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Crush;
+use Carbon\Carbon;
 
 class CrushesController extends Controller
 {
@@ -25,7 +26,11 @@ class CrushesController extends Controller
      */
     public function create()
     {
-        //
+        $crush = new Crush();
+        return view('crushes.create', array('crush'       =>$crush,
+                                            'action'      =>route('crushes.store'),
+                                            'submit_text' =>"Create Crush")
+    );
     }
 
     /**
@@ -36,7 +41,10 @@ class CrushesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $crush = new Crush();
+       $this->setAndSaveCrushData($crush, $request);
+
+        return redirect()->route('crushes.index');
     }
 
     /**
@@ -58,7 +66,11 @@ class CrushesController extends Controller
      */
     public function edit($id)
     {
-        //
+        $crush = Crush::find($id);
+        return view('crushes.create', array('crush'       =>$crush,
+                                            'action'      =>route('crushes.id.update', array('id'=>$crush->id) ),
+                                            'submit_text' =>"Update Crush")
+    );
     }
 
     /**
@@ -70,7 +82,9 @@ class CrushesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $crush = Crush::find($id);
+        $this->setAndSaveCrushData($crush, $request);
+        return redirect() ->route('crushes.index');
     }
 
     /**
@@ -81,6 +95,20 @@ class CrushesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $crush = Crush::find($id);
+        $crush->delete();
+
+        return redirect()->back();
     }
+
+private function setAndSaveCrushData($crush, $request){
+        $crush->first_name = $request->first_name;
+        $crush->last_name = $request->last_name;
+        $crush->fb_profile_link = $request->fb_profile_link;
+        $crush->contact_number = $request->contact_number;
+        $crush->created_at = Carbon::now();
+        $crush->updated_at = Carbon::now();
+        $crush->save();
+}
+
 }
--- laravelFirstWebsite/resources/views/crushes/index.blade.php
+++ laravelFirstWebsite/resources/views/crushes/index.blade.php
@@ -3,8 +3,9 @@
         <tr>
             <td> First Name </td>
             <td> Last Name </td>
-            <td><a href="https://www.facebook.com/ninyahmae.pacatang"> FB Profile</a> </td>
+            <td> FB Profile </td>
             <td> Contact Number </td>
+            <td> Action </td>
         </tr>
     </thead>
     <tbody>
@@ -14,7 +15,15 @@
             <td> {{ $crush -> last_name     }} </td>
             <td>  {{ $crush -> fb_profile_link     }} </td>
             <td> {{ $crush -> contact_number     }} </td>
+            <td> <a href="{{ route('crushes.id.edit', array ('id'=>$crush->id)) }}"> Edit </a> <a href="{{ route('crushes.id.destroy', array ('id'=>$crush->id)) }}"> Delete </a></td>
         </tr>
         @endforeach
+        <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td> <a href="{{ route('crushes.create') }}">Add New Crush</a></td>
+        </tr>
     </tbody>
 </table>
